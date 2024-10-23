092958fc00ba4b644b8739a20828c436de9e81ef
mohamed <mohamed@utopios.net>
2024-10-23 20:35:29 +0200

    add form reactive  debut

--- demo-docapost-angular/src/app/pages/formulaire/formulaire.component.ts
+++ demo-docapost-angular/src/app/pages/formulaire/formulaire.component.ts
@@ -1,17 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './formulaire.component.html',
-  styleUrl: './formulaire.component.css'
+  styleUrl: './formulaire.component.css',
 })
 export class FormulaireComponent {
+  name = 'Angular';
 
-name= 'Angular';
+  onTemplateSubmit() {
+    console.log('onTemplateSubmit', this.user);
+  }
 
+  user: any = {
+    email: '',
+    password: '',
+    address: '',
+    city: '',
+  };
+
+  reactiveForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.reactiveForm = this.fb.group({
+      email: ['', [Validators.required], Validators.email],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')]],
+      address: ['', Validators.required],
+      city: ['', Validators.required, Validators.minLength(5)],
+    });
+  }
+  onReactiveSubmit() {
+
+    console.log('onReactiveSubmit', this.reactiveForm.value);
+  }
 
 }
