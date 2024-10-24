698fd3c2b0405e7627eafcc37e824254a3364326
mohamed <mohamed@utopios.net>
2024-10-24 19:59:17 +0200

    add demo service

--- demo-docapost-angular/src/app/components/nav/nav.component.ts
+++ demo-docapost-angular/src/app/components/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TitleService } from '../../utils/services/title.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
