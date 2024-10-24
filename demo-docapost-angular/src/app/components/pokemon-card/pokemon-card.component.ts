45722732022b2ac345c365351eed227a8a013aaf
mohamed <mohamed@utopios.net>
2024-10-24 20:35:00 +0200

    add demo parent

---
+++ demo-docapost-angular/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.css'
+})
+export class PokemonCardComponent {
+
+@Input() pokemon: string = 'poke';
+
+
+}
--- demo-docapost-angular/src/app/pages/parent/parent.component.ts
+++ demo-docapost-angular/src/app/pages/parent/parent.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { TitleService } from '../../utils/services/title.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { PokemonCardComponent } from '../../components/pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule,
+     FormsModule, 
+     PokemonCardComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
